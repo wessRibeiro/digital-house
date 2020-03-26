80ea378a0c90e8ed2c7af2d306dd9a281ff06f10
Weslley Ribeiro <wess_ribeiro@hotmail.com>
2020-03-25 22:16:53 -0300

    aula de laco

---
+++ 25_03_2020/lacos.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Created by Weslley Ribeiro.
+ * User: Weslley Ribeiro <wess_ribeiro@hotmail.com>
+ * Date 25/03/2020 20:02
+ */
+
+//while - enquanto
+//enquanto algo for verdade faça isso  pra mim
+
+/*$contador = 0;
+while ($contador <= 5){
+    echo "contador vale: $contador <br/>";
+    $contador++;
+}*/
+
+/*
+//primeiro faça depois pergunte
+$i = 0;
+do {
+    echo $i;
+} while ($i > 0);
+*/
+
+//é o mais complexo, porém tbm é o mais completo
+/*for ($i = 1; $i <= 10; $i++) {
+    echo $i;
+}
+*/
+
+//coleção, muitos, varios, conjunto, legiao
+//$arrs = range(0, 1000);
+
+//percorra, percorre os indices
+/*foreach ($arrs as $item) {
+    echo "$item<br/>";
+}*/
+/*$aluno =
+    [
+        'nome' => 'lira',
+        'idade' => 18,
+        'rg' => '1234567'
+    ];
+
+echo '<pre>';
+foreach ($aluno as $key => $value) {
+    if($key == 'nome'){
+        if($value == 'lira'){
+            echo "{$key} : {$value} <br/>";
+            break;
+        }
+
+    }
+
+echo '</pre>';
+*/
+
+
+/*
+if ($i == 0) {
+    echo "i equals 0";
+} elseif ($i == 1) {
+    echo "i equals 1";
+} elseif ($i == 2) {
+    echo "i equals 2";
+}*/
+
+/*$aluno =
+    [
+        'nome' => 'lira',
+        'idade' => 18,
+        'rg' => '1234567'
+    ];
+*/
+
+/*
+echo '<pre>';
+foreach ($aluno as $key => $value) {
+    if ($key == 'nome') {
+        if ($value == 'weslley') {
+            echo "{$key} : {$value} <br/>";
+            break;
+        }
+
+    }
+}
+*/
+
+/*
+$i = 2;
+
+switch ($i) {
+    case 0:
+        echo "i equals 0";
+        break;
+    case 1:
+        echo "i equals 1";
+        break;
+    case 2:
+        echo "i equals 2";
+        break;
+    case 3:
+        echo "i equals 3";
+        break;
+    case 4:
+        echo "i equals 4";
+        break;
+    case 5:
+        echo "i equals 5";
+        break;
+}
+*/
